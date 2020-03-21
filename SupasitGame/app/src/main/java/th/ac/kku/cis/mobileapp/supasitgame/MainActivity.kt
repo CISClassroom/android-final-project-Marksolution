7ba833f6094a844dd7557220f443a7005dd85b62
Marksolution <33540488+Marksolution@users.noreply.github.com>
2020-03-22 01:19:48 +0700

    login success

--- SupasitGame/app/src/main/java/th/ac/kku/cis/mobileapp/supasitgame/MainActivity.kt
+++ SupasitGame/app/src/main/java/th/ac/kku/cis/mobileapp/supasitgame/MainActivity.kt
@@ -1,12 +1,44 @@
 package th.ac.kku.cis.mobileapp.supasitgame
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
-
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        if (supportActionBar != null)
+            supportActionBar?.hide()
+
+        val NameSetting: TextView = findViewById(R.id.name)
+        val Profile: ImageView = findViewById(R.id.imageProfile)
+        val Email: TextView = findViewById(R.id.email_)
+        auth = FirebaseAuth.getInstance()
+        auth.currentUser!!.email
+        val xx: Uri? = auth.currentUser!!.photoUrl
+        NameSetting.text = auth.currentUser!!.displayName.toString()
+        Picasso.get().load(xx).into(Profile)
+
+        Email.text = auth.currentUser!!.email
+        val btnlogout: Button = findViewById(R.id.logoutmain)
+
+        btnlogout.setOnClickListener {
+
+            var i = Intent(this, Login_main::class.java)
+            i.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+            startActivity(i)
+        }
     }
+
+
+
 }
+
